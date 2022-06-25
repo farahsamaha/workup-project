1e7635e060825fbf1ac5d27c9eacc4fc99fcb7ea
farahsamaha <farahsamaha121@gmail.com>
2022-06-25 15:26:00 +0300

    create auth components

---
+++ resources/js/Components/Auth/AuthenticationCardLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <inertia-link :href="'/'">
+    <v-img
+      width="14rem"
+      class="mx-auto logo"
+      src="../../assets/auth.png"
+    >
+    </v-img>
+  </inertia-link>
+</template>
