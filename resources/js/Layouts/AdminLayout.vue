5bfce9f6e3411de537abb3f390e0f2627e937969
farahsamaha <farahsamaha121@gmail.com>
2022-06-29 16:27:16 +0300

    fix folder typo

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
+       <v-app-bar
       app
       light
       elevation="0"
@@ -28,30 +28,118 @@
         <v-icon v-if="!$vuetify.theme.dark"> mdi-weather-night </v-icon>
         <v-icon v-else> mdi-white-balance-sunny </v-icon>
       </v-btn>
-      <!-- <v-app-bar-nav-icon @click="drawer = !drawer" /> -->
+            <v-app-bar-nav-icon
+        v-if="$vuetify.breakpoint.mobile"
+        @click="drawer = !drawer"
+      />    <v-navigation-drawer
+      app
+      dark
+      color="drawer"
+      v-model="drawer"
+      width="300"
+      :mini-variant="mini && !$vuetify.breakpoint.mobile"
+      :expand-on-hover="mini && !$vuetify.breakpoint.mobile"
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-group>
+          <template #activator>
+            <v-list-item
+              class="px-0"
+              style="height: 60px;"
+            >
+              <v-list-item-avatar size="24">
+                <v-img :src="auth.user.avatar" />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ auth.user.name }}
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ auth.user.email }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+          <Link
+            as="v-list-item"
+            :href="route('logout')"
+            method="post"
+          >
+          <v-list-item-icon>
+            <v-icon>mdi-lock</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item-content>
+          </Link>
+        </v-list-group>
+      </v-list>
+
+      <v-divider />
+
+      <v-list
+        dense
+        nav
+      >
+        <Link
+          as="v-list-item"
+          v-for="(item, key) in menu"
+          :key="key"
+          link
+          :input-value="item.active"
+        >
+        <v-list-item-icon>
+          <v-icon v-html="item.icon" />
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.text" />
+        </v-list-item-content>
+        </Link>
+      </v-list>
+    </v-navigation-drawer>
     </v-app-bar>
 
-    <side-bar :key="route().current()" />
-    <v-main style="background-color: #ececec">
-      <flash-messages />
+    <v-main class="mb-10">
+  <flash-messages />
       <slot />
     </v-main>
-
-    <v-footer app small>
-      &copy; {{ new Date().getFullYear() }} {{ $page.props.app.name }}
-    </v-footer>
+    <footer-sec />
   </v-app>
 </template>
 
 <script>
-import FlashMessages from "@/components/Layout/FlashMessages";
-import SideBar from "@/components/Layout/SideBar";
-// import FooterSec from "@/Components/Layout/FooterSec.vue";
+import FlashMessages from "@/components/partials/FlashMessages";
+import SideBar from "@/components/partials/SideBar";
+import FooterSec from "@/Components/partials/FooterSec.vue"
 
 export default {
   components: {
     FlashMessages,
     SideBar,
+    FooterSec
   },
-};
+  computed: {
+    title () {
+      return this.$page.props.title
+    },
+    auth () {
+      return this.$page.props.auth
+    }
+  },
+  data () {
+    return {
+      drawer: true,
+      mini: true,
+      menu: [
+        {
+          icon: mdiViewDashboardOutline,
+          text: 'Dashboard'
+        }
+      ]
+    }
+  }
+}
 </script>
