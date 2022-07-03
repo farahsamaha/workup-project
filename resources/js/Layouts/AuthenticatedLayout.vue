5144cfdd5924d20a2c9164361d6295457fe07ea3
farahsamaha <farahsamaha121@gmail.com>
2022-07-04 00:06:09 +0300

    edit layouts

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,172 @@
+<template>
+  <v-app>
+    <v-app-bar app elevation="0" height="60px" fixed>
+      <v-app-bar-title>
+        <v-btn elevation="0" text height="40" width="150">
+          <v-img
+            class="mx-2 ml-5"
+            src="/assets/logo2.png"
+            max-height="100"
+            max-width="130"
+            contain
+          ></v-img
+        ></v-btn>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <!-- <v-bottom-navigation
+    :value="value"
+    color="teal"
+    horizontal
+  > -->
+      <v-btn text class="mx-4" color="teal">
+        <v-icon>mdi-history</v-icon>
+        <span>Recents</span>
+      </v-btn>
+
+      <v-btn text class="mx-4" color="teal">
+        <v-icon>mdi-plus-box</v-icon>
+        <span>Add post</span>
+      </v-btn>
+
+      <v-btn text class="mr-9" color="teal">
+        <v-icon>mdi-wallet-travel</v-icon>
+        <span>Jobs</span>
+      </v-btn>
+      <v-responsive max-width="260">
+        <v-text-field
+          prepend-inner-icon="mdi-magnify x-large"
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+        ></v-text-field>
+      </v-responsive>
+      <v-btn
+        icon
+        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+        class="teal--text"
+      >
+        <v-icon v-if="!$vuetify.theme.dark"> mdi-weather-night </v-icon>
+        <v-icon v-else> mdi-white-balance-sunny </v-icon>
+      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-navigation-drawer app v-model="drawer" absolute temporary right>
+        <v-list nav dense>
+          <v-list-item class="px-0" style="height: 60px">
+            <v-list-item-avatar size="24">
+              <v-img :src="auth.user.avatar" />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ auth.user.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ auth.user.email }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider class="ml-3 mr-3"></v-divider>
+          <v-list-item-group v-model="group" active-class="text--accent-4">
+            <v-list-item v-for="item in items" :key="item.title" link>
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn
+              block
+              class="teal darken-2 white--text"
+              :href="route('logout')"
+              method="post"
+            >
+              <v-icon>mdi-lock</v-icon>
+              Logout
+            </v-btn>
+          </div>
+        </template>
+        <!-- <v-list nav dense>
+            <v-list dense nav>
+              <Link
+                as="v-list-item"
+                v-for="(item, key) in menu"
+                :key="key"
+                link
+                :input-value="item.active"
+              >
+                <v-list-item-icon>
+                  <v-icon v-html="item.icon" />
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text" />
+                </v-list-item-content>
+              </Link>
+            </v-list>
+          </v-list-item-group>
+        </v-list> -->
+      </v-navigation-drawer>
+    </v-app-bar>
+    <v-main class="mb-10">
+      <flash-messages />
+      <slot />
+    </v-main>
+    <footer-sec />
+  </v-app>
+</template>
+
+<script>
+import FlashMessages from "@/components/partials/FlashMessages";
+import SideBar from "@/components/partials/SideBar";
+import FooterSec from "@/components/partials/FooterSec.vue";
+
+export default {
+  data() {
+    return {
+      links: ["Messages", "Profile", "Add Post"],
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Account", icon: "mdi-account-box" },
+        { title: "Admin", icon: "mdi-gavel" },
+      ],
+    };
+  },
+  components: {
+    FlashMessages,
+    SideBar,
+    FooterSec,
+  },
+  computed: {
+    title() {
+      return this.$page.props.title;
+    },
+    auth() {
+      return this.$page.props.auth;
+    },
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+  data() {
+    return {
+      drawer: false,
+      group: null,
+      mini: true,
+      menu: [
+        {
+          text: "Dashboard",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      elevation="0"
+      height="60px"
+      fixed
+      scroll-target="#scrolling-techniques"
+    >
+      <v-btn elevation="0" text height="40" width="150" :href="route('home')">
+        <v-img
+          class="mx-2 ml-5"
+          src="/assets/logo2.png"
+          max-height="100"
+          max-width="130"
+          contain
+        >
+        </v-img
+      ></v-btn>
+      <v-spacer></v-spacer>
+      <div v-if="canLogin" class="fixed top-0 right-0 px-6 py-4 sm:block">
+        <v-row>
+          <!-- <v-btn
+            class="d-flex justify-center justify-space-around mr-3 teal--text"
+            large
+            text
+            height="35"
+            v-if="$page.props.auth.user"
+            :href="route('dashboard')"
+          >
+            Dashboard
+          </v-btn> -->
+          <v-btn
+            class="d-flex justify-center justify-space-around mr-3 teal--text"
+            large
+            text
+            height="35"
+            :href="route('login')"
+          >
+            Log in
+          </v-btn>
+          <v-btn
+            class="d-flex justify-center justify-space-around mr-3 teal--text"
+            large
+            text
+            height="35"
+            v-if="canRegister"
+            :href="route('register')"
+          >
+            Register</v-btn
+          >
+        </v-row>
+      </div>
+      <v-btn
+        icon
+        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+        class="teal--text"
+      >
+        <v-icon v-if="!$vuetify.theme.dark"> mdi-weather-night </v-icon>
+        <v-icon v-else> mdi-white-balance-sunny </v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main class="mb-12">
+      <slot />
+    </v-main>
+    <footer-sec />
+  </v-app>
+</template>
+
+<script>
+import FooterSec from "@/components/partials/FooterSec.vue";
+
+export default {
+  components: { FooterSec },
+  props: {
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+  },
+
+  data() {
+    return {
+      showingNavigationDropdown: false,
+    };
+  },
+
+  methods: {
+    logout() {
+      this.$inertia.post(route("logout"));
+    },
+  },
+};
+</script>
