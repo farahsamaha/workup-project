31812d469ba3276ea707a783407cb362d6b7d6ae
ulfat-yabroudi <ulfatyab.4@gmail.com>
2022-07-05 16:27:26 +0300

    create user components

---
+++ resources/js/Components/User/PersonalInfo.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-card class="pa-2" outlined tile>
+        <v-list-item-content class="ml-5">
+            <v-list-item-title class="title">
+                <h1>Nour Aldeen Sh</h1>
+                <br />
+            </v-list-item-title>
+            <h2>About me</h2>
+        </v-list-item-content>
+        <v-row class="fill-height">
+            <v-card-title>
+                <v-btn dark icon color="teal-accent-4" class="mr-4">
+                    <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-btn dark icon color="teal-accent-4">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+            </v-card-title>
+        </v-row>
+        <v-spacer></v-spacer>
+
+        <v-list two-line>
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-icon color="teal-accent-4"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-action></v-list-item-action>
+
+                <v-list-item-content>
+                    <v-list-item-title>4/4/1998</v-list-item-title>
+                    <v-list-item-subtitle>Birthday</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-icon color="teal-accent-4"> mdi-phone </v-icon>
+                </v-list-item-icon>
+                <v-list-item-action></v-list-item-action>
+
+                <v-list-item-content>
+                    <v-list-item-title>(963) 944877638</v-list-item-title>
+                    <v-list-item-subtitle>Mobile</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-icon>
+                    <v-icon color="teal-accent-4">mdi-message-text</v-icon>
+                </v-list-item-icon>
+            </v-list-item>
+
+            <v-divider inset></v-divider>
+
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-icon color="teal-accent-4"> mdi-email </v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title
+                        >nouralddin@example.com</v-list-item-title
+                    >
+                    <v-list-item-subtitle>Personal</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-action></v-list-item-action>
+
+                <v-list-item-content>
+                    <v-list-item-title
+                        >nour_alddin@example.com</v-list-item-title
+                    >
+                    <v-list-item-subtitle>Work</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider inset></v-divider>
+
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-icon color="teal-accent-4"> mdi-map-marker </v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title>Mazzeh</v-list-item-title>
+                    <v-list-item-subtitle>Damascus, Syria</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-card>
+</template>
---
+++ resources/js/Components/User/UserCv.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-card class="pa-2" outlined tile>
+        <v-list>
+            <v-list-group
+                v-for="item in items"
+                :key="item.title"
+                v-model="item.active"
+                :prepend-icon="item.action"
+                color="teal-accent-4"
+                no-action
+            >
+                <template v-slot:activator>
+                    <v-list-item-content>
+                        <v-list-item-title
+                            v-text="item.title"
+                        ></v-list-item-title>
+                    </v-list-item-content>
+                </template>
+
+                <v-list-item v-for="child in item.items" :key="child.title">
+                    <v-list-item-content>
+                        <v-list-item-title
+                            v-text="child.title"
+                        ></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-group>
+        </v-list>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        items: [
+            {
+                action: "mdi-ticket",
+                active: true,
+                items: [
+                    { title: "Communication" },
+                    { title: "Research" },
+                    { title: "English" },
+                    { title: "Arabic" },
+                ],
+                title: "Skills",
+            },
+            {
+                action: "mdi-school",
+                items: [{ title: "Software Engineering" }],
+                title: "Education",
+            },
+            {
+                action: "mdi-briefcase",
+                items: [{ title: "3 Years of teaching" }],
+                title: "Experience",
+            },
+            {
+                action: "mdi-bottle-tonic-plus",
+                items: [{ title: "At Kona Wa Sanbaa" }],
+                title: "Volunteering",
+            },
+        ],
+    }),
+};
+</script>
