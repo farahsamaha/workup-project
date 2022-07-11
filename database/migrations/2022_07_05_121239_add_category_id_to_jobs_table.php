b15c5933d5fa10d12e8d542cb02f707cabf193af
farahsamaha <farahsamaha121@gmail.com>
2022-07-12 01:09:57 +0300

    edit one to many relations

--- database/migrations/2022_07_05_121239_add_category_id_to_jobs_table.php
+++ database/migrations/2022_07_05_121239_add_category_id_to_jobs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('jobs', function (Blueprint $table) {
-            $table->foreignId('category_id');
+            $table->foreignId('category_id')->after('title');
         });
     }
 
