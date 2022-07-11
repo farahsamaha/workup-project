77a72c6527e0e2dda09c5395a3d5a876d8d87d1f
farahsamaha <farahsamaha121@gmail.com>
2022-07-12 02:18:46 +0300

    edit job categories

--- resources/js/Components/Home/JobCategories.vue
+++ resources/js/Components/Home/JobCategories.vue
@@ -3,14 +3,14 @@
     <v-row
     >
       <v-col cols="12">
-        <div class="text-h2 text-center my-5 teal--text">Job Categories</div>
+        <div class="text-h2 text-center my-5 teal--text">Popular Categories</div>
       </v-col>
-      <v-col v-for="(Category, i) in categories" :key="i" cols="3">
+      <v-col v-for="(Category, i) in categories" :key="i" cols="4">
         <v-hover v-slot="{ hover }">
           <v-card
             class="mx-auto"
-            max-width="344"
-            max-height="360"
+            width="344"
+            height="300"
             :class="{ scale: hover }"
           >
             <v-img :src="Category.src" height="200px"></v-img>
@@ -20,14 +20,6 @@
             <v-card-subtitle>
               {{ Category.subtitle }}
             </v-card-subtitle>
-            <v-btn
-              class="teal accent-4 white--text ma-3"
-              elevation="2"
-              rounded
-              x-small
-              tile
-              >{{ Category.count }}</v-btn
-            >
           </v-card>
         </v-hover>
       </v-col>
@@ -44,7 +36,7 @@ export default {
         title: 'Information Technology',
         subtitle:
           'web developer / data analysist / graphic designer / software engineering',
-        count: '20'
+
       },
 
       {
@@ -52,7 +44,7 @@ export default {
         title: 'Economy',
         subtitle:
           'accountant / HR manager/ Banking / sales manager / supply chain ',
-        count: '32'
+
       },
 
       {
@@ -60,13 +52,7 @@ export default {
         title: 'Adminstiration Mangement',
         subtitle:
           'data entry / publishing / archiving / translation / customer services  / Logistic ',
-        count: '19'
-      },
-      {
-        src: '/assets/other.jpg',
-        title: 'Others',
-        subtitle: 'Secertaria/ Reseptionest  / writing editorial / negotietor',
-        count: '11'
+
       }
     ]
   })
