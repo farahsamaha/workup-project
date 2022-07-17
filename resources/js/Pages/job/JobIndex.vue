56de0bd9b0a9670252082d7a9123568734868b40
farahsamaha <farahsamaha121@gmail.com>
2022-07-17 21:13:09 +0300

    add pagination

--- resources/js/Pages/job/JobIndex.vue
+++ resources/js/Pages/job/JobIndex.vue
@@ -1,14 +1,27 @@
 <template>
   <v-container>
-       <!-- <div class="container">
+    <!-- <div class="container">
       <div v-if="Message" class="alert alert-success mt-4">
         {{ Message }}
       </div>
        </div> -->
 
     <v-col>
-        <search-filter/>
+      <search-filter />
+
+      <!-- <div v-for="job in jobs" :key="job.id"> -->
       <job-card />
+      <!-- </div> -->
+      <div class="text-center">
+        <v-pagination
+          color="orange"
+          class="mt-16"
+          v-model="page"
+          :length="4"
+          :links="jobs.links"
+          circle
+        ></v-pagination>
+      </div>
     </v-col>
   </v-container>
 </template>
@@ -16,11 +29,16 @@
 <script>
 import JobLayout from "../../layouts/JobLayout";
 import JobCard from "@/components/job/JobCard.vue";
-import SearchFilter from "@/components/job/SearchFilter.vue"
+import SearchFilter from "@/components/job/SearchFilter.vue";
 export default {
   layout: JobLayout,
-  components: { JobCard , SearchFilter },
-   props: ['jobs', 'Message'],
+  components: { JobCard, SearchFilter },
+  data() {
+    return {
+      page: 1,
+    };
+  },
+  props: ["jobs", "Message"],
 };
 </script>
 
