87892facecb79b87c6e01f701a8d77caea62454c
farahsamaha <farahsamaha121@gmail.com>
2022-07-20 21:24:04 +0300

    add search filter using model scope

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,20 +8,42 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'category_id', 'location_id', 'email', 'description'];
+    public $timestamps = false;
 
     public function categories()
     {
-        return $this->hasMany(Category::class);
+        return $this->belongsToMany(Category::class, 'categories');
     }
-
     public function locations()
     {
-        return $this->hasMany(Location::class);
+        return $this->belongsToMany(Location::class, 'locations');
     }
-    public function types() {
-        return $this->belongsToMany(Type::class);
+    public function types()
+    {
+        return $this->belongsToMany(Type::class, 'types');
     }
-    public function places() {
-        return $this->belongsToMany(Place::class);
+    public function places()
+    {
+        return $this->belongsToMany(Place::class, 'places');
+    }
+
+    public function scopeFilter($query, array $filters)
+    {
+        if ($filters['category'] ?? false) {
+            $query->where('categories', 'like', '%' . request('category') . '%');
+        }
+
+        if ($filters['location'] ?? false) {
+            $query->where('locations', 'like', '%' . request('location') . '%');
+        }
+
+        if ($filters['types'] ?? false) {
+            $query->where('types', 'like', '%' . request('types') . '%');
+        }
+
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%');
+        }
     }
 }
