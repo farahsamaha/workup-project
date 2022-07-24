b081fc1059ffe7e9f89f5befb8d4b192b7b7ee84
ulfat-yabroudi <ulfatyab.4@gmail.com>
2022-07-24 20:33:56 +0300

    uodate userCv

--- resources/js/Components/User/UserCv.vue
+++ resources/js/Components/User/UserCv.vue
@@ -1,65 +1,75 @@
 <template>
-    <v-card class="pa-2" outlined tile>
-        <v-list>
-            <v-list-group
-                v-for="item in items"
-                :key="item.title"
-                v-model="item.active"
-                :prepend-icon="item.action"
-                color="teal-accent-4"
-                no-action
-            >
-                <template v-slot:activator>
-                    <v-list-item-content>
-                        <v-list-item-title
-                            v-text="item.title"
-                        ></v-list-item-title>
-                    </v-list-item-content>
-                </template>
+  <v-card class="pa-2" outlined tile>
+    <v-list>
 
-                <v-list-item v-for="child in item.items" :key="child.title">
-                    <v-list-item-content>
-                        <v-list-item-title
-                            v-text="child.title"
-                        ></v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-group>
-        </v-list>
-    </v-card>
+      <v-list-group prepend-icon="mdi-ticket" color="teal-accent-4" no-action>
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>Skills</v-list-item-title>
+          </v-list-item-content>
+        </template>
+        <v-list-item v-for="skill in skills.data" :key="skill.id">
+          {{user.skill_id}}
+          <v-list-item-content>
+            <v-list-item-title v-text="child.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+
+        <v-list-group prepend-icon="mdi-school" color="teal-accent-4" no-action>
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>Education</v-list-item-title>
+          </v-list-item-content>
+        </template>
+        <v-list-item v-for="certificate in certificate.data" :key="certificate.id">
+          {{user.certificate_id}}
+          <v-list-item-content>
+            <v-list-item-title v-text="child.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+
+      <v-list-group prepend-icon="mdi-briefcase" color="teal-accent-4" no-action>
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>Experience</v-list-item-title>
+          </v-list-item-content>
+        </template>
+        <v-list-item v-for="experience in experience.data" :key="experience.id">
+          {{user.experience_id}}
+          <v-list-item-content>
+            <v-list-item-title v-text="child.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+
+      <v-list-group prepend-icon="mdi-bottle-tonic-plus" color="teal-accent-4" no-action>
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>Volunteering</v-list-item-title>
+          </v-list-item-content>
+        </template>
+        <v-list-item v-for="organization in organization.data" :key="organization.id">
+          {{user.organization_id}}
+          <v-list-item-content>
+            <v-list-item-title v-text="child.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        // items: [
-        //     {
-        //         action: "mdi-ticket",
-        //         active: true,
-        //         items: [
-        //             { title: "Communication" },
-        //             { title: "Research" },
-        //             { title: "English" },
-        //             { title: "Arabic" },
-        //         ],
-        //         title: "Skills",
-        //     },
-        //     {
-        //         action: "mdi-school",
-        //         items: [{ title: "Software Engineering" }],
-        //         title: "Education",
-        //     },
-        //     {
-        //         action: "mdi-briefcase",
-        //         items: [{ title: "3 Years of teaching" }],
-        //         title: "Experience",
-        //     },
-        //     {
-        //         action: "mdi-bottle-tonic-plus",
-        //         items: [{ title: "At Kona Wa Sanbaa" }],
-        //         title: "Volunteering",
-        //     },
-        // ],
-    }),
+  props:{
+     users :Object,
+     skills: Array,
+     certificates: Array,
+     experiences: Array,
+     organizations: Array,
+  }
+
 };
 </script>
